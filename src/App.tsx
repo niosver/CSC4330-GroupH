98fe6ef5df722b67ac34191498ea7edd7a49a9f5
alex <alxford45@gmail.com>
2021-02-13 19:00:04 -0600

    readded home and router

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,26 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Login } from "./pages/Login";
+import { Home } from "./pages/Home";
+import { Navbar } from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-function App() {
-  return <Login />;
-}
+const App: React.FC = () => {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
