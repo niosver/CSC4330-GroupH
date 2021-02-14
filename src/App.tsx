785db8910a40fb8d95e94ddeba3ec96ebe51c1d2
alex <alxford45@gmail.com>
2021-02-13 18:52:13 -0600

    reformatted files

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {Login} from "./pages/Login"
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Login } from "./pages/Login";
 
 function App() {
-  return (
-    <Login />
-  )
-  
+  return <Login />;
 }
 
 export default App;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,69 @@
 import React from "react";
+import { Button, TextField } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: 200,
+    },
+  },
+}));
+
+
 
 export const Login: React.FC = () => {
+    const classes = useStyles();
 
-    return (
-    <div>login</div>
-    );
-}
+  return (
+    <form className={classes.root} noValidate autoComplete="off">
+      <div>
+        <TextField error id="standard-error" label="Error" defaultValue="Hello World" />
+        <TextField
+          error
+          id="standard-error-helper-text"
+          label="Error"
+          defaultValue="Hello World"
+          helperText="Incorrect entry."
+        />
+      </div>
+      <div>
+        <TextField
+          error
+          id="filled-error"
+          label="Error"
+          defaultValue="Hello World"
+          variant="filled"
+        />
+        <TextField
+          error
+          id="filled-error-helper-text"
+          label="Error"
+          defaultValue="Hello World"
+          helperText="Incorrect entry."
+          variant="filled"
+        />
+      </div>
+      <div>
+        <TextField
+          error
+          id="outlined-error"
+          label="Error"
+          defaultValue="Hello World"
+          variant="outlined"
+        />
+        <TextField
+          error
+          id="outlined-error-helper-text"
+          label="Error"
+          defaultValue="Hello World"
+          helperText="Incorrect entry."
+          variant="outlined"
+        />
+      </div>
+      <Button type="submit">Submit  </Button>
+    </form>
+  );
+
+};
