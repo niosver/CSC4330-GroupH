180611e1ea1fcc17f4b246c4bd362fb197d4f10e
Timothy Lee <tlee4224@yahoo.com>
2021-02-13 19:12:16 -0600

    shared session changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {Login} from "./pages/Login"
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Login } from "./pages/Login";
+import { Home } from "./pages/Home";
+import { Navbar } from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <Login />
-  )
-  
-}
+    <Router>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
