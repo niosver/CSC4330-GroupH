2671bdd3b48d1f005c7a41aa38e0bcf0a8821152
alex <alxford45@gmail.com>
2021-02-15 20:25:32 -0600

    extended eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  extends: [
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'prettier',
+    'prettier/react',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
+  ],
+  plugins: ['react', '@typescript-eslint', 'jest'],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  rules: {
+    'react/prop-types': 'off',
+    'jsx-a11y/anchor-is-valid': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'import/prefer-default-export': 'off',
+    'linebreak-style': 'off',
+    'no-console': 'off',
+    'no-explicit-any': 'off',
+    'no-underscore-dangle': 'off',
+    'prettier/prettier': 'warn',
+    '@typescript-eslint/naming-convention': 'warn'
+  },
+};
