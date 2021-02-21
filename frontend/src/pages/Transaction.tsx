8d26b354ce2d85d5c167e948e697642cef10b4d0
Timothy Lee <tlee4224@yahoo.com>
2021-02-20 19:46:06 -0600

    feat: Transaction page dropdown addition

--- frontend/src/pages/Transaction.tsx
+++ frontend/src/pages/Transaction.tsx
@@ -3,47 +3,94 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
 
+const times = [
+    {
+        value: '30',
+        label: '30 minutes',
+    },
+    {
+        value: '1',
+        label: '1 Hour',
+    },
+    {
+        value: '2',
+        label: '2 Hours',
+    },
+    {
+        value: '3',
+        label: '3 Hours +',
+    },
+];
 
 const useStyles = makeStyles((theme) => ({
     root: {
-      display: 'flex',
-      flexDirection: 'column',
-      minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+    },
+    dropdown: {
+        margin: theme.spacing(1),
+        width: '25ch',
     },
     main: {
-      marginTop: theme.spacing(8),
-      marginBottom: theme.spacing(2),
+        marginTop: theme.spacing(8),
+        marginBottom: theme.spacing(2),
     },
     footer: {
-      padding: theme.spacing(3, 2),
-      marginTop: 'auto',
-      backgroundColor:
-        theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
+        padding: theme.spacing(3, 2),
+        marginTop: 'auto',
+        backgroundColor:
+            theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
     },
-  }));
+}));
 
 export const Transaction: React.FC = () => {
-  const classes = useStyles();
+    const classes = useStyles();
+    const [time, setCurrency] = React.useState('1');
 
-  return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <Container component="main" className={classes.main} maxWidth="sm">
-        <Typography variant="h2" component="h1" gutterBottom>
-          Sticky footer
-        </Typography>
-        <Typography variant="h5" component="h2" gutterBottom>
-          {'Pin a footer to the bottom of the viewport.'}
-          {'The footer will move as the main element of the page grows.'}
-        </Typography>
-        <Typography variant="body1">Sticky footer placeholder.</Typography>
-      </Container>
-      <footer className={classes.footer}>
-        <Container maxWidth="sm">
-          <Typography variant="body1">My sticky footer can be found here.</Typography>
-        </Container>
-      </footer>
-    </div>
-  );
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setCurrency(event.target.value);
+    };
+
+    return (
+        <div className={classes.root}>
+            <CssBaseline />
+            <Container component="main" className={classes.main} maxWidth="lg">
+                <Typography variant="h4" align="center" component="h1" gutterBottom>
+                    Bike Transaction
+                </Typography>
+            </Container>
+            <Container component="main" className={classes.main} maxWidth="lg">
+                <form className={classes.root} noValidate autoComplete="off">
+                    <div>
+                        <TextField
+                            id="time-rented"
+                            select
+                            label="Rent Time"
+                            value={time}
+                            onChange={handleChange}
+                            SelectProps={{
+                                native: true,
+                            }}
+                            helperText="Please select time rented"
+                        >
+                            {times.map((option) => (
+                                <option key={option.value} value={option.value}>
+                                    {option.label}
+                                </option>
+                            ))}
+                        </TextField>
+                    </div>
+                </form>
+            </Container>
+            <footer className={classes.footer}>
+                <Container maxWidth="sm">
+                    <Typography variant="body1">My sticky footer can be found here.</Typography>
+                </Container>
+            </footer>
+        </div>
+    );
 };
