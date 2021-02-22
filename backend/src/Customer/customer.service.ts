d47755c3e57e3718079afebfc9d7e27e6dce8d1d
niosver <raladnier@hotmail.com>
2021-02-22 11:55:17 -0600

    created Customer class and imported it to customer service

--- backend/src/Customer/customer.service.ts
+++ backend/src/Customer/customer.service.ts
@@ -1,6 +1,10 @@
 import express from "express";
+import {Customer} from "./customer.interface"
 
 var router = express.Router();
 
+router.post("/create_customer", async function(req,res) {
+    res.send("Not implemented yet.")
+});
 
-module.exports = router
+export default router
