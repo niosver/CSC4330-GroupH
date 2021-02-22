ee0fc68fb15adb6fa77bc514f576dcbcf5c0bb02
niosver <raladnier@hotmail.com>
2021-02-22 11:54:44 -0600

    added 2 create account methods

--- backend/src/Account/account.service.ts
+++ backend/src/Account/account.service.ts
@@ -1,14 +1,43 @@
 import express from "express";
-import {Account} from "./account.interface"
+import {Account_Type,Account} from "./account.interface"
+import {Customer} from "../Customer/customer.interface"
 
 var router = express.Router();
+const create_customer = "INSERT INTO Customer(?,?,?,?,?,?,?,?)"
+const create_account = "INSERT INTO Account(?,?,?,?)"
 
 router.post('/signup', async function(req,res) {
-    res.send("Not implemented")
+    let db = req.app.locals.db;
+    try{
+        await db.beginTransaction();
+        let json = req.body;
+        let newC = new Customer(json.first_name,json.last_name,json.address,json.birthdate,json.email,json.cc_number,json.cc_name,json.billing_address);
+        let customer = await db.query(create_customer,[newC.first_name,newC.last_name,newC.address,newC.birthdate,newC.email,newC.cc_number,newC.cc_name,newC.billing_address]);
+        let newA = new Account(json.username,json.password,Account_Type.customer,customer.customer_id);
+        await db.query(create_account,[newA.username,newA.password,newA.account_type,newA.customer_id]);
+        db.commit();
+        res.status(200).send("OK");
+    }
+    catch{
+        db.rollback();
+        res.status(400).send("Error Creating new customer");
+    }
 });
 
 router.post('/create_manager', async function(req,res) {
-    res.send("Not implemented");
+    let db = req.app.locals.db;
+    try{
+        await db.beginTransaction();
+        let json = req.body;
+        let newA = new Account(json.username,json.password,Account_Type.manager);
+        await db.query(create_account,[newA.username,newA.password,newA.account_type,newA.customer_id]);
+        db.commit();
+        res.status(200).send("OK");
+    }
+    catch{
+        db.rollback();
+        res.status(400).send("Error creating new account");
+    }
 });
 
 router.get('/login', async function(req,res) {
