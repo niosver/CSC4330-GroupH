1a25710c43961623344c6273ea597ecd17f079c0
alex <alxford45@gmail.com>
2021-03-18 15:56:36 -0500

    small fixes in POST /rent; documented error in POST /return

--- backend/src/Transaction/transaction.service.ts
+++ backend/src/Transaction/transaction.service.ts
@@ -18,16 +18,18 @@ const add_fee = "UPDATE Transaction SET damage_fee=? WHERE transaction_id=?;";
 router.post("/rent", async function (req, res, next) {
 	let db = req.app.locals.db;
 	let json = req.body;
+	let result;
 	try {
 		let id = req.session.customer_id;
 		if (id == undefined) {
 			res.status(400).send("Missing account info for rental.");
 			return next();
 		}
-		let cc_num = await db.query(get_cc_number, [id]);
+		result = await db.query(get_cc_number, [id]);
+		const cc_num = result[0].cc_number;
 		let now = new Date();
 		let trans = new Transaction(cc_num, now, id, json.dock);
-		let result = await db.query(insert_trans, [
+		result = await db.query(insert_trans, [
 			trans.price,
 			trans.cc_number,
 			trans.start_date,
@@ -35,34 +37,61 @@ router.post("/rent", async function (req, res, next) {
 			trans.origin_dock,
 			trans.status,
 		]);
-		let trans_id = result.insertId;
+		const trans_id = result.insertId;
 		if (trans_id >= 0) {
 			if (req.session.current_transactions == undefined) {
 				req.session.current_transactions = [];
 			}
 			req.session.current_transactions.push(trans_id);
-			res.status(200).send(trans_id);
+			res.status(200).send({ transaction_id: trans_id });
 		} else throw Error;
 	} catch (error) {
 		Logger.log(error, Logger.ERROR);
 		res.status(400).send("Unable to process rental");
 	}
 });
+/*
+Current implementation of storing transactions in memory does not work. For example:
+	1. user logs in as user1 => session: {customer_id: 1, transactions: []}
+	2. user1 rents bike from dock 1 => session: {customer_id: 1, transactions: [3]}
+	3. user1 logs out  => session: undefined
+	4. user1 logs back in => session: {customer_id: 1, transactions: []}
+	5. user1 returns bike to dock 1 => {customer_id: 1, transactions: []} => [] does not contain 3
+	
+Fix1: remove transactions entirely from session and just query in this function
+	- await `SELECT * FROM Transaction WHERE transaction_id=id1, customer_id=id2, status=in_progress`
 
+Fix2: initialize transactions in session inside login function
+	- req.session.current_transaction = await `SELECT * FROM Transaction WHERE customer_id=id, status=in_progress`
+	- may be complicated to add `if customer` check
+	- adds additional error case for login if transaction query fails
+
+*/
 router.post("/return", async function (req, res, next) {
 	let db = req.app.locals.db;
 	let json = req.body;
 	let now = new Date();
+	let result;
 	try {
-		let e = req.session.current_transactions?.indexOf(json.transaction_id);
-		if (e && e != -1) {
+		const request_trans_id = parseInt(json.transaction_id, 10); // extra safety in case transaction_id is sent as string
+
+		/*
+		current_trans_id = [5,4,3].indexOf(5)				// 0 because 5 is first element in array
+		current_trans_id = [5,4,3].includes(x) ? x : -1 	// x when array contains x or -1 when array does not contain x
+		*/
+		// const current_trans_id = req.session.current_transactions?.indexOf(request_trans_id);
+		const current_trans_id = req.session.current_transactions?.includes(request_trans_id)
+			? request_trans_id
+			: -1;
+		if (current_trans_id && current_trans_id != -1) {
 			db.beginTransaction();
-			let trans = await db.query(get_trans, [json.transaction_id]);
+			result = await db.query(get_trans, [json.transaction_id]);
+			const trans = result[0];
 			let u_trans = new Transaction(
-				trans[0].cc_number,
-				trans[0].start_date,
-				trans[0].customer_id,
-				trans[0].origin_dock
+				trans.cc_number,
+				trans.start_date,
+				trans.customer_id,
+				trans.origin_dock
 			);
 			u_trans.set_end_date(now);
 			u_trans.set_destination(json.destination_dock);
@@ -75,10 +104,15 @@ router.post("/return", async function (req, res, next) {
 				u_trans.status,
 				json.transaction_id,
 			]);
-			req.session.current_transactions?.splice(e, 1);
+			req.session.current_transactions?.splice(current_trans_id, 1);
 			db.commit();
-			res.status(200).send(price);
-		} else throw Error;
+			res.status(200).send({ price: price });
+		} else {
+			const message = current_trans_id
+				? `value of current_trans_id: ${current_trans_id}`
+				: "current_trans_id is undefined";
+			throw new Error(message);
+		}
 	} catch (error) {
 		db.rollback();
 		res.status(400).send("Unable to process return");
