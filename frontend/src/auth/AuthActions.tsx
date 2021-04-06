267fd39d0779216a3bbebe51c563331b180eed91
alex <alxford45@gmail.com>
2021-04-06 12:15:09 -0500

    added loading context to auth module

---
+++ frontend/src/auth/AuthActions.tsx
@@ -0,0 +1,23 @@
+import type { Dispatch } from 'react';
+import { AuthActionType } from 'types/Auth';
+import type { AuthAction } from 'types/Auth';
+
+/**
+ * Wrapper for dispatching change in AuthLoadingState setting loading to true.
+ *
+ * @param {Dispatch<ActionType>} dispatch
+ */
+export const beginLoading = (dispatch: Dispatch<AuthAction>) => {
+    dispatch({ type: AuthActionType.BEGIN_LOADING });
+};
+
+const MINIMUM_LOADING_TIME = 750; // minimum loading time for smooth transition
+
+/**
+ * Wrapper for dispatching change in AuthLoadingState setting loading to false.
+ *
+ * @param {Dispatch<ActionType>} dispatch
+ */
+export const endLoading = (dispatch: Dispatch<AuthAction>) => {
+    setTimeout(() => dispatch({ type: AuthActionType.END_LOADING }), MINIMUM_LOADING_TIME);
+};
