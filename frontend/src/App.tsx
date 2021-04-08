5eed6ba9c3b2d713075d4c2ae34ee3059f3035e4
alex <alxford45@gmail.com>
2021-04-08 14:34:03 -0500

    added new DashStyles context to Dashboard & App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { PageSpinner } from 'components/PageSpinner';
 import { Landing, SignIn, SignUp } from 'pages';
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { DashStylesProvider } from 'context/styles';
 import './App.css';
 import { Dashboard } from './pages/Dashboard';
 import { Routes } from './Routes';
@@ -13,44 +14,46 @@ const App: React.FC = () => {
     return (
         <Router>
             <AuthProvider>
-                <Switch>
-                    <Route exact path="/">
-                        <Landing />
-                    </Route>
-                    <Route path="/signin">
-                        <SignIn />
-                    </Route>
-                    <Route path="/signup">
-                        <SignUp />
-                    </Route>
-                    {/* AUTHENTICATED ROUTES BEGIN*/}
-                    {Routes.map((route, idx) => (
-                        <AuthGuard
-                            path={route.path}
-                            redirect="/"
-                            authRedirect="/dashboard/home"
-                            key={idx}
-                            account_type={route.account_type}
-                        >
-                            <Dashboard>
-                                <route.content />
-                            </Dashboard>
-                        </AuthGuard>
-                    ))}
-                    {/* AUTHENTICATED ROUTES END*/}
+                <DashStylesProvider>
+                    <Switch>
+                        <Route exact path="/">
+                            <Landing />
+                        </Route>
+                        <Route path="/signin">
+                            <SignIn />
+                        </Route>
+                        <Route path="/signup">
+                            <SignUp />
+                        </Route>
+                        {/* AUTHENTICATED ROUTES BEGIN*/}
+                        {Routes.map((route, idx) => (
+                            <AuthGuard
+                                path={route.path}
+                                redirect="/"
+                                authRedirect="/dashboard/home"
+                                key={idx}
+                                account_type={route.account_type}
+                            >
+                                <Dashboard>
+                                    <route.content />
+                                </Dashboard>
+                            </AuthGuard>
+                        ))}
+                        {/* AUTHENTICATED ROUTES END*/}
 
-                    {/* DEV ROUTES BEGIN*/}
-                    <Route path="/dev/dashboard">
-                        <Dashboard />
-                    </Route>
-                    <Route path="/dev/transaction">
-                        <Transaction />
-                    </Route>
-                    <Route path="/dev/spinner">
-                        <PageSpinner />
-                    </Route>
-                    {/* DEV ROUTES END */}
-                </Switch>
+                        {/* DEV ROUTES BEGIN*/}
+                        <Route path="/dev/dashboard">
+                            <Dashboard />
+                        </Route>
+                        <Route path="/dev/transaction">
+                            <Transaction />
+                        </Route>
+                        <Route path="/dev/spinner">
+                            <PageSpinner />
+                        </Route>
+                        {/* DEV ROUTES END */}
+                    </Switch>
+                </DashStylesProvider>
             </AuthProvider>
         </Router>
     );
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -17,104 +17,24 @@ import { useAuth } from 'auth';
 import clsx from 'clsx';
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import { useDashStyles } from 'context/styles';
 import { UserPublic } from 'types/User';
 import { NavDrawer } from '../components/NavDrawer';
 
-const drawerWidth = 240;
-
-function preventDefault(event: { preventDefault: () => void }) {
-    event.preventDefault();
-}
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-    },
-    toolbar: {
-        paddingRight: 24, // keep right padding when drawer closed
-    },
-    toolbarIcon: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'flex-end',
-        padding: '0 8px',
-        ...theme.mixins.toolbar,
-    },
-    appBar: {
-        zIndex: theme.zIndex.drawer + 1,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-    },
-    appBarShift: {
-        marginLeft: drawerWidth,
-        width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    },
-    menuButton: {
-        marginRight: 36,
-    },
-    menuButtonHidden: {
-        display: 'none',
-    },
-    title: {
-        flexGrow: 1,
-    },
-    drawerPaper: {
-        position: 'relative',
-        whiteSpace: 'nowrap',
-        width: drawerWidth,
-        transition: theme.transitions.create('width', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    },
-    drawerPaperClose: {
-        overflowX: 'hidden',
-        transition: theme.transitions.create('width', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-        width: theme.spacing(7),
-        [theme.breakpoints.up('sm')]: {
-            width: theme.spacing(9),
-        },
-    },
-    appBarSpacer: theme.mixins.toolbar,
-    content: {
-        flexGrow: 1,
-        height: '100vh',
-        overflow: 'auto',
-    },
-    container: {
-        paddingTop: theme.spacing(4),
-        paddingBottom: theme.spacing(4),
-    },
-    paper: {
-        padding: theme.spacing(2),
-        display: 'flex',
-        overflow: 'auto',
-        flexDirection: 'column',
-    },
-    fixedHeight: {
-        height: 240,
-    },
-}));
+// function preventDefault(event: { preventDefault: () => void }) {
+//     event.preventDefault();
+// }
 
 export const Dashboard: React.FC = ({ children }) => {
-    const classes = useStyles();
-    const [open, setOpen] = React.useState(true);
+    const classes = useDashStyles();
+    const [open, setOpen] = React.useState(false);
     const handleDrawerOpen = () => {
         setOpen(true);
     };
     const handleDrawerClose = () => {
         setOpen(false);
     };
-    const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+    // const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
     const auth = useAuth();
     const history = useHistory();
 
@@ -192,15 +112,15 @@ export const Dashboard: React.FC = ({ children }) => {
                     {/* BEGIN CONTENT*/}
                     <Grid container spacing={3}>
                         <Grid item xs={12}>
-                            <Paper className={classes.paper}>{children}</Paper>
+                            {children}
                         </Grid>
                     </Grid>
                     {/* END CONTENT*/}
-                    <Box pt={4}>
+                    {/* <Box pt={4}>
                         <Link color="primary" href="#" onClick={preventDefault}>
                             About us
                         </Link>
-                    </Box>
+                    </Box> */}
                 </Container>
             </main>
         </div>
