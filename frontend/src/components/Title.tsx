3bed29e6b42fd961152005176334a329925a4270
alex <alxford45@gmail.com>
2021-04-08 16:44:04 -0500

    minor adjustment

--- frontend/src/components/Title.tsx
+++ frontend/src/components/Title.tsx
@@ -1,16 +1,19 @@
-import Typography from '@material-ui/core/Typography';
+import Typography, { TypographyProps } from '@material-ui/core/Typography';
 import React from 'react';
 
-export default function Title(props: { children: React.ReactNode }) {
+type Props = TypographyProps;
+const Title: React.FC<Props> = ({ children }, props) => {
     return (
         <Typography
             component="h2"
             variant="h6"
             color="primary"
             gutterBottom
+            {...props}
             style={{ paddingLeft: 16, paddingBottom: 16 }} // temp styling adjustment
         >
-            {props.children}
+            {children}
         </Typography>
     );
-}
+};
+export default Title;
