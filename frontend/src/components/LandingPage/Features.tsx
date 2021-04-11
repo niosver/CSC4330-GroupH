681b9660d9c62f15a5c14cf03bc54d52be0d0943
alex <alxford45@gmail.com>
2021-04-10 20:16:04 -0500

    added 'count up' animation for stats;  general  styling changes

--- frontend/src/components/LandingPage/Features.tsx
+++ frontend/src/components/LandingPage/Features.tsx
@@ -1,40 +1,36 @@
-import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardHeader from '@material-ui/core/CardHeader';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-
-import feature_img from '../../assets/feature_img.png';
+import Typography from '@material-ui/core/Typography';
+import { useIntersection, useInterval } from 'hooks';
+import React, { useEffect, useRef, useState } from 'react';
+import feature_img from '../../assets/strengths_img.png';
 
 const useStyles = makeStyles((theme) => ({
-    '@global': {
-        ul: {
-            margin: 0,
-            padding: 0,
-            listStyle: 'none',
-        },
-    },
     paper: {
         position: 'relative',
         backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat',
         backgroundImage: `url(${feature_img})`,
-        marginBottom: theme.spacing(5),
+        height: '40vh',
     },
     heroContent: {
         padding: theme.spacing(6, 0, 6),
+    },
+    cardHeader: {
+        display: 'flex',
+        justifyContent: 'center',
     },
     cardContent: {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'baseline',
         marginBottom: theme.spacing(4),
+        color: 'inherit',
     },
     sectionTitle: {
         color: 'white',
@@ -42,59 +38,93 @@ const useStyles = makeStyles((theme) => ({
     gridSize: {
         width: '75%',
     },
+    overlay: {
+        backgroundColor: 'rgba(0,0,0,.6)',
+        height: 'inherit',
+    },
+    cardRoot: {
+        background: 'transparent',
+        color: 'white',
+    },
+    container: {
+        flexGrow: 1,
+        position: 'relative',
+        transform: 'translateY(60%)',
+    },
 }));
 
-const strengths = [
+const stats = [
     {
-        feature: '10 Docks with 3 bikes',
-        description: ["Description of feature and why it's special"],
+        number: 10,
+        unit: 'Locations in service',
     },
     {
-        feature: '30 bikes to choose from',
-        description: ["Description of feature and why it's special"],
+        number: 30,
+        unit: 'Bikes to choose from',
     },
     {
-        feature: 'x years of experience',
-        description: ["Description of feature and why it's special"],
+        number: 12,
+        unit: 'years of experience',
     },
 ];
 
 export default function Strengths() {
     const classes = useStyles();
-    console.log(feature_img);
+    const [start, setStart] = useState(false);
+    const counts = stats.map((stat) => useInterval(stat.number, start));
+    const ref = useRef<HTMLDivElement | null>(null);
+    const inViewport = useIntersection(ref);
+
+    useEffect(() => {
+        console.log('viewport: ', inViewport);
+        if (inViewport) {
+            setStart(true);
+        }
+    }, [inViewport]);
+    useEffect(() => {
+        console.log('start: ', start);
+    }, [start]);
     return (
         <React.Fragment>
             <CssBaseline />
             {/* Hero unit */}
             <Paper className={classes.paper}>
-                <Container maxWidth="sm" component="main" className={classes.heroContent}>
-                    <Typography className={classes.sectionTitle} align="center" variant="h3">
-                        Bike App Features
-                    </Typography>
-                </Container>
-                {/* End hero unit */}
-                <Grid container spacing={10} alignItems="flex-end">
-                    {strengths.map((strength) => (
-                        // Enterprise card is full width at sm breakpoint
-                        <Grid item key={strength.feature} xs={12} sm={6} md={4}>
-                            <Card>
-                                <CardHeader />
-                                <CardContent>
-                                    <div className={classes.cardContent}>
-                                        <Typography
-                                            component="h2"
-                                            variant="h4"
-                                            color="textPrimary"
-                                            align="center"
-                                        >
-                                            {strength.feature}
-                                        </Typography>
-                                    </div>
-                                </CardContent>
-                            </Card>
-                        </Grid>
-                    ))}
-                </Grid>
+                <div className={classes.overlay}>
+                    {/* <Container maxWidth="sm" component="main" className={classes.heroContent}>
+                        <Typography className={classes.sectionTitle} align="center" variant="h3">
+                            Bike App Features
+                        </Typography>
+                    </Container> */}
+                    {/* End hero unit */}
+                    <Grid
+                        container
+                        className={classes.container}
+                        direction="row"
+                        justify="center"
+                        alignItems="center"
+                        ref={ref}
+                    >
+                        {stats.map((strength, idx) => (
+                            // Enterprise card is full width at sm breakpoint
+                            <Grid item key={idx} xs={3} sm={3} md={3} lg={3}>
+                                <Card className={classes.cardRoot} elevation={0}>
+                                    <CardHeader
+                                        className={classes.cardHeader}
+                                        title={counts[idx]}
+                                        titleTypographyProps={{ align: 'center', variant: 'h3' }}
+                                    />
+                                    <CardContent>
+                                        <div className={classes.cardContent}>
+                                            <Typography variant="h5" align="center">
+                                                {strength.unit}
+                                            </Typography>
+                                        </div>
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+                        ))}
+                    </Grid>
+                </div>
             </Paper>
         </React.Fragment>
     );
